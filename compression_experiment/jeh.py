b98ac29bcf4174a5177e0d2efacaf6c4d9a27db4
Benaiah662 <benaiahjehoiada@protonmail.com>
2025-08-10 09:56:27 +1000

    added Jeh.py (brinstorm file)

---
+++ compression_experiment/jeh.py
@@ -0,0 +1,29 @@
+#Declaring Variables
+reply = 0 #User reply to input prompt
+int = 0 #Converted Text As Intiger
+formula = ("abc", "12c3", "12c3", "12cqwce", "casdw") #Formula as result
+remaining_digits  = 0 #Remaining digits after calculation
+
+
+
+
+def func1(int): #Rem stands for remaining digits
+    if int >= 10**20:
+        #calc math formula
+        formula = formula + ("first num", "second num")
+        
+
+
+
+
+
+
+#other random stuff/brainstorming
+
+input("1)Encode 2)Decode 3)Settings: ")
+
+func1(int) #Call function with Rem as argument
+
+print(formula[0] + "^" + formula[1], end='\r') #Replys to user
+
+
